ed20886dfacff4d2286e68c557261c289fe0b407
Gabriel Nadolny <39236516+InteractiveNinja@users.noreply.github.com>
2020-03-10 11:00:17 +0100

    Added Hotkeys

--- nice.py
+++ nice.py
@@ -1,13 +1,21 @@
 import clipboard
-text = clipboard.paste()
-lenght = len(text)
-output = []
-for i in range(lenght):
-    if (i % 2) == 0:
-        output.append(text[i].upper())
-    else:
-        output.append(text[i])
-        
-clipboard.copy(''.join(output))
+import keyboard
+import time
+def makeNice():
+    text = clipboard.paste()
+    lenght = len(text)
+    output = []
+    for i in range(lenght):
+        if (i % 2) == 0:
+            output.append(text[i].upper())
+        else:
+            output.append(text[i])
+            
+    keyboard.write("".join(output))
 
 
+keyboard.add_hotkey('ctrl+shift+ü',makeNice)
+keyboard.add_hotkey('ctrl+shift+q',exit)
+
+while True:
+    time.sleep(1)
